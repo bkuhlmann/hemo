04f5eb9cb008e7353c57a5ef989ff46a1fda1ec6
Brooke Kuhlmann <brooke@alchemists.io>
2023-01-17 17:26:41 -0700

    Added tasks feature spec
    Necessary to test the entire stack and demonstrate the full capabilities of the application.

---
+++ spec/features/tasks_spec.rb
@@ -0,0 +1,89 @@
+# frozen_string_literal: true
+
+require "hanami_helper"
+
+RSpec.describe "Tasks", :db, :js, :web do
+  let(:user) { Factory[:user, name: "Jane Doe", email: "jdoe@example.com"] }
+  let(:task) { Factory[:task, user:, description: "A test task"] }
+  let(:tasks_path) { Hanami.app["routes"].path :tasks_root }
+
+  it "displays no tasks when there are none" do
+    visit tasks_path
+    expect(page).to have_content("Congratulations")
+  end
+
+  it "displays task" do
+    task
+    visit tasks_path
+    expect(page).to have_content("A test task")
+  end
+
+  it "successfully creates a new task" do
+    user
+    visit tasks_path
+    click_on "New"
+
+    select "Jane Doe", from: "task[user_id]"
+    fill_in "task[description]", with: "This is a test"
+    click_on "Save"
+
+    expect(page).to have_content("This is a test")
+  end
+
+  it "fails to create task with errors" do
+    user
+    visit tasks_path
+    click_on "New"
+
+    select "Jane Doe", from: "task[user_id]"
+    click_on "Save"
+
+    expect(page).to have_content("must be filled")
+  end
+
+  it "returns to index when canceling new task" do
+    visit tasks_path
+    click_on "New"
+    click_on "Cancel"
+    sleep 0.5  # For htmx swap delay.
+
+    expect(page).to have_no_content("Cancel")
+  end
+
+  it "successfully completes a task" do
+    task
+    visit tasks_path
+    check "complete"
+
+    expect(find(".task-completed").text).to include(task.description)
+  end
+
+  it "successfully edits a task" do
+    task
+    visit tasks_path
+    click_on "Edit"
+    fill_in "task[description]", with: "An edited task"
+    click_on "Save"
+
+    expect(page).to have_content("An edited task")
+  end
+
+  it "is unable to edit a task with errors" do
+    task
+    visit tasks_path
+    click_on "Edit"
+    fill_in "task[description]", with: nil
+    click_on "Save"
+
+    expect(page).to have_content("must be filled")
+  end
+
+  it "successfully destroys a task" do
+    task
+    visit tasks_path
+    click_on "Delete"
+    sleep 0.3  # For htmx swap delay.
+
+    expect(page).to have_no_content(task.description)
+  end
+end
