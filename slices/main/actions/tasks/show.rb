7fc0a523d3d07b877433886ccf31d69d609ada8c
Brooke Kuhlmann <brooke@alchemists.io>
2023-01-19 10:54:49 -0700

    Added main task show action
    Necessary to render a single task.

---
+++ slices/main/actions/tasks/show.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Main
+  module Actions
+    module Tasks
+      # Processes show action.
+      class Show < Main::Action
+        include Deps[repository: "repositories.task"]
+
+        params { required(:id).filled :integer }
+
+        def handle request, response
+          parameters = request.params
+          halt :unprocessable_entity unless parameters.valid?
+          response.render view, task: repository.find(parameters[:id])
+        end
+      end
+    end
+  end
+end
---
+++ slices/main/views/tasks/show.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Main
+  module Views
+    module Tasks
+      # Renders main show view.
+      class Show < Main::View
+        expose :task
+      end
+    end
+  end
+end
