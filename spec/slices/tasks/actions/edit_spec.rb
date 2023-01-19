fe726fa8771c13555ef32be65db2aef5107a5002
Brooke Kuhlmann <brooke@alchemists.io>
2023-01-19 10:58:02 -0700

    Added tasks edit action
    Necessary to allow an existing task to be edited with new information for eventual update.

---
+++ spec/slices/tasks/actions/edit_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "hanami_helper"
+
+RSpec.describe Tasks::Actions::Edit, :db do
+  subject(:action) { described_class.new }
+
+  describe "#call" do
+    it "answers 200 OK status with valid parameters" do
+      response = action.call id: Factory[:task].id
+      expect(response.status).to eq(200)
+    end
+
+    it "answers errors with invalid parameters" do
+      response = action.call Hash.new
+      expect(response.status).to eq(422)
+    end
+  end
+end
