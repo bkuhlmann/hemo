772892f43edb7653ead7565edfe87d828d171e6f
Brooke Kuhlmann <brooke@alchemists.io>
2023-01-19 10:48:22 -0700

    Added task routes
    Necessary to default the root route to the tasks index along with all other task CRUD actions. This ties all of the previous work together so when you launch the application you'll immediately be able to interact with the tasks.

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,17 @@ module Hemo
   class Routes < Hanami::Routes
     slice(:health, at: "/up") { root to: "show" }
     slice(:home, at: "/") { root to: "show" }
+
+    slice :tasks, at: "/tasks" do
+      root to: "index"
+
+      get "/:id", to: "show", as: :show
+      get "/new", to: "new", as: :new
+      post "/", to: "create", as: :create
+      get "/:id/edit", to: "edit", as: :edit
+      patch "/:id", to: "patch", as: :patch
+      put "/:id", to: "update", as: :update
+      delete "/:id", to: "delete", as: :delete
+    end
   end
 end
