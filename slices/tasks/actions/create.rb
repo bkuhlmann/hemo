54dc793b085d18a9c9e807b1e896899033235df1
Brooke Kuhlmann <brooke@alchemists.io>
2023-01-19 10:56:59 -0700

    Added tasks create action
    Necessary to create and persist a task to the database.

---
+++ slices/tasks/actions/create.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module Tasks
+  module Actions
+    # The create action.
+    class Create < Tasks::Action
+      include Deps[
+        task_repository: "repositories.task",
+        user_repository: "repositories.user",
+        show_view: "views.show",
+        new_view: "views.new"
+      ]
+
+      params do
+        required(:task).hash do
+          required(:user_id).filled :integer
+          required(:description).filled :string
+        end
+      end
+
+      def handle request, response
+        parameters = request.params
+
+        if parameters.valid?
+          task = task_repository.create parameters[:task]
+          response.render show_view, task: task_repository.find(task.id)
+        else
+          response.render new_view, users: user_repository.all, error: parameters.errors[:task]
+        end
+      end
+    end
+  end
+end
