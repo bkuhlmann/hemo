b6b7c02424986518c4e89595d7be036096b70780
Brooke Kuhlmann <brooke@alchemists.io>
2023-08-12 14:36:04 -0600

    Added tasks skeleton
    Necessary to provide the initial skeleton for building task functionality by using a dedicated slice.

---
+++ slices/tasks/action.rb
@@ -0,0 +1,8 @@
+# auto_register: false
+# frozen_string_literal: true
+
+module Tasks
+  # The tasks action.
+  class Action < Hemo::Action
+  end
+end
---
+++ slices/tasks/repository.rb
@@ -0,0 +1,8 @@
+# auto_register: false
+# frozen_string_literal: true
+
+module Tasks
+  # The tasks repository.
+  class Repository < Hemo::Repository
+  end
+end
