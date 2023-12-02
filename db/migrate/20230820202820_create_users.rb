7c1c1dab53a371a41bf445b2fc1b6005ce019145
Brooke Kuhlmann <brooke@alchemists.io>
2023-12-02 15:22:33 -0700

    Added users migration
    Necessary to establish a users table for which to eventually assign ownership to a task.

---
+++ db/migrate/20230820202820_create_users.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+ROM::SQL.migration do
+  change do
+    create_table :users do
+      primary_key :id, :bigint
+      column :name, :text, null: false
+      column :email, :text, null: false
+      column :created_at, :timestamp, null: false, default: Sequel::CURRENT_TIMESTAMP
+      column :updated_at, :timestamp, null: false, default: Sequel::CURRENT_TIMESTAMP
+    end
+  end
+end
