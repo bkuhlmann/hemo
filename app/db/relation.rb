afaaad0ce973ea622facfb76d1afc82b916e097a
Brooke Kuhlmann <brooke@alchemists.io>
2024-03-02 08:04:18 -0700

    Added project skeleton
    Generated with link:https://alchemists.io/projects/hanamismith[Hanamismith] 1.9.0.

---
+++ app/db/relation.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "hanami/db/relation"
+
+module Hemo
+  module DB
+    # The application database base relation.
+    class Relation < Hanami::DB::Relation
+    end
+  end
+end
